12f7e3d570db6f00868f987d357782bbe638d46f
Roy Li <me@dada.li>
2018-12-30 17:38:08 +0800

    feat: render markdown command

---
+++ app/controller/api.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const axios = require('axios');
+const validator = require('validator');
+
+module.exports = app => {
+  return class ApiController extends app.Controller {
+    async html() {
+      const { ctx } = this;
+      const url = ctx.query.url;
+
+      if (!url || !validator.isURL(url)) {
+        ctx.throw(400);
+        return;
+      }
+
+      const response = await axios({
+        method: 'get',
+        url,
+        responseType: 'stream',
+        proxy: false,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        },
+      });
+      const fileStream = response.data;
+      const contentType = response.headers['content-type'];
+
+      if (!contentType || !contentType.startsWith('text/html')) {
+        ctx.throw(400);
+        return;
+      }
+
+      ctx.set('content-type', contentType);
+      ctx.body = fileStream;
+    }
+  };
+};
--- app/router.js
+++ app/router.js
@@ -1,10 +1,12 @@
 'use strict';
 
 module.exports = app => {
-  const { router } = app;
+  const { router, controller } = app;
 
   router.post(`/bot${app.config.bot.botToken}`, async function(ctx) {
     ctx.app.bot.processUpdate(ctx.request.body);
     ctx.status = 200;
   });
+
+  router.get('/html', controller.api.html);
 };
