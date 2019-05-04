0e4bd8fca329030287452715ff191a796767e01b
Roy Li <me@dada.li>
2019-05-04 23:15:45 +0800

    feat: change logs output in production

--- config/config.prod.js
+++ config/config.prod.js
@@ -7,5 +7,11 @@ module.exports = () => {
     timeout: 5000,
   };
 
+  config.logger = {
+    consoleLevel: 'INFO',
+    level: 'NONE',
+    disableConsoleAfterReady: false,
+  };
+
   return config;
 };
